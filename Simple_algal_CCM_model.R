5c8dde0f9e4d265780d05c2e00b9acc229a667a5
Liat Adler <liatadler@hotmail.com>
2020-07-18 20:54:21 +0100

    basic code for shiny app(needs renaming to app.R)

---
+++ Simple_algal_CCM_model.R
@@ -0,0 +1,47 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Parameters"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            h4('Set Models Parameter Below'), 
+            # Set drainage times for each pool
+            sliderInput("F1", "F1: carbon influx", 
+                        min = 1, max = 100, value = 50, step= 1),
+            
+            sliderInput("F3", "F1: carbon eflux", 
+                        min = 1, max = 100, value = 50, step= 1)
+        ),
+
+        # meant to show number for CCM simple formula
+        mainPanel(
+            textOutput("CCM")
+        )
+    )
+)
+
+# Define server logic required to calculate the C13 discrimination
+#I'm defining the function to calcualtre the formula below
+server <- function(input, output) {
+       output$CCM <- renderText({
+           -7.9+27*(input$F3/input$F1)
+       })
+} 
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
